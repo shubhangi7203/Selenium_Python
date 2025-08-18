5f357023355019f06514b60f2df3eb0625cc24bd
shubhangi7203 <94372711+shubhangi7203@users.noreply.github.com>
2025-08-18 16:13:25 +0530

    Update run_all_tests.py

--- My_Selenium_Program/com/qa/testscripts/run_all_tests.py
+++ My_Selenium_Program/com/qa/testscripts/run_all_tests.py
@@ -4,6 +4,13 @@ import glob
 # Get all .py files from current directory (excluding run_all_tests.py)
 files = [f for f in glob.glob("*.py") if f != "run_all_tests.py"]
 
+pytest_args = [
+    current_dir,                            # Run all .py files in this folder
+    "--junitxml=../../../../result.xml",    # Save JUnit report (for Jenkins Test Result Trend)
+    "--html=../../../../report.html",       # Save HTML report
+    "--self-contained-html"                 # Make HTML report standalone (no external CSS/JS)
+]
+
 print(f"Running the following files:\n{files}\n")
 
 for file in files:
